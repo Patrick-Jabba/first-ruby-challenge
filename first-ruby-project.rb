799420562b483c58c89a60bce470795b5e0c256a
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2023-03-10 18:19:14 -0300

    :tada: Desafio conceito de ruby

---
+++ first-ruby-project.rb
@@ -0,0 +1,16 @@
+# Entrada de dados na tela
+puts"======= Seja bem vindo ao cadastrado da Biblioteca Nacional do Brasil ======="
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+# Saída de dados
+puts "#################"
+puts "Carregando dados..."
+puts "#################"
+puts "Cadastro de #{nome} #{sobrenome}, #{idade} anos realizado com sucesso."
